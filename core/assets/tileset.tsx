0d575b70c6f04d391ccbe96859292a1137013cdd
Miro Koczka <usbecko@gmail.com>
2018-04-05 07:46:57 +0200

    feat(project): add libgdx framework

---
+++ core/assets/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="129" tileheight="129" spacing="1" tilecount="465" columns="31">
+ <image source="./spritesheet_tiles.png" width="4096" height="2048"/>
+</tileset>
---
+++ desktop/src/com/koczka/gameofpatterns/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.koczka.gameofpatterns.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.koczka.gameofpatterns.GameOfPatterns;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameOfPatterns(), config);
+	}
+}
